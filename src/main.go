d214420f69f2c78f4eea6369d9d59410a28492ca
TrashPony <TrashPony13@gmail.com>
2017-09-24 21:18:43 +0300

    fix web static files

--- src/main.go
+++ src/main.go
@@ -11,13 +11,10 @@ func main() {
 
 	router := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir("src/static/")) // эээ файловый сервер для раздачи статики который нехуя не работает так как разные библиотеки
-	router.Handle("/", fs) // гуглить mux fileServer
-	router.HandleFunc("/ws", auth.HandleConnections) // если браузер запрашивает соеденение на /ws то инициализируется переход на вебсокеты
-	go auth.HandleMessages() // запускается гарутина для рассылки сообщений, гуглить гарутины
-
-	router.HandleFunc("/", nil)
 	router.HandleFunc("/login", auth.Login) // если заходят на /login то отрабатывает функция auth.Login
+	router.HandleFunc("/ws", auth.HandleConnections) // если браузер запрашивает соеденение на /ws то инициализируется переход на вебсокеты
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./src/static/"))) // раздача статичный файлов
+	go auth.HandleMessages() // запускается гарутина для рассылки сообщений, гуглить гарутины
 	log.Println("http server started on :8080")
 	http.ListenAndServe(":8080", router) // запускает веб сервер на 8080 порту
 }
