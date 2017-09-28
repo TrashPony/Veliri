6974a6cbcccf5ee7d4c29029a0038a6f8fc707b1
TrashPony <TrashPony13@gmail.com>
2017-09-28 13:16:41 +0300

    rename files

--- src/webSocket/utils.go
+++ src/webSocket/utils.go
@@ -3,40 +3,8 @@ package webSocket
 import (
 	"websocket-master"
 	"log"
-	"net/http"
-	"strconv"
 )
-var upgrader = websocket.Upgrader{} // методами приема обычного HTTP-соединения и обновления его до WebSocket
-var usersLobbyWs = make(map[Clients]bool) // тут будут храниться наши подключения
-var usersFieldWs = make(map[Clients]bool) // тут будут храниться наши подключения
-var LobbyPipe = make(chan LobbyResponse)
-var FieldPipe = make(chan FieldResponse)
 
-func ReadSocket(login string, id int, w http.ResponseWriter, r *http.Request, pool string)  {
-
-	ws, err := upgrader.Upgrade(w, r, nil) // запрос GET для перехода на протокол websocket
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if pool == "/wsLobby" {
-		usersLobbyWs[Clients{ws, login, id}] = true // Регистрируем нового Клиента
-		print("WS lobby Сессия: ") // просто смотрим новое подключение
-		print(ws)
-		println(" login: " + login + " id: " + strconv.Itoa(id))
-		defer ws.Close() // Убедитесь, что мы закрываем соединение, когда функция возвращается (с) гугол мужик
-		LobbyReader(ws)
-	}
-	if pool == "/wsField" {
-		usersFieldWs[Clients{ws, login, id}] = true // Регистрируем нового Клиента
-		print("WS field Сессия: ") // просто смотрим новое подключение
-		print(ws)
-		println(" login: " + login + " id: " + strconv.Itoa(id))
-		defer ws.Close() // Убедитесь, что мы закрываем соединение, когда функция возвращается (с) гугол мужик
-		FieldReader(ws)
-	}
-
-}
 
 func LoginWs(ws *websocket.Conn, usersWs *map[Clients]bool) (string)  {
 	for client := range *usersWs { // ходим по всем подключениям
@@ -56,7 +24,6 @@ func DelConn(ws *websocket.Conn, usersWs *map[Clients]bool, err error)  {
 		}
 	}
 }
-
 
 type Clients struct { // структура описывающая клиента ws соеденение
 	ws *websocket.Conn
