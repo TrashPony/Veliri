5e8028e7468d309c0ef382c706040d279e8d9089
TrashPony <TrashPony13@gmail.com>
2017-11-12 20:31:18 +0300

    fix del unit

--- src/webSocket/field/readyUser.go
+++ src/webSocket/field/readyUser.go
@@ -83,12 +83,13 @@ func attack(sortUnits []objects.Unit, activeUser []*Clients) {
 					if target.X == x && target.Y == y {
 						sortUnits[i].Hp = target.Hp - unit.Damage
 						if sortUnits[i].Hp <= 0 {
-							go mechanics.DelUnit(sortUnits[i].Id)
+							mechanics.DelUnit(sortUnits[i].Id)
 							// TODO оповещалка об атаки в сокет
 							DelUnit(sortUnits[i], activeUser)
 						} else {
-							go mechanics.UpdateUnit(sortUnits[i].Id, sortUnits[i].Hp)
+							mechanics.UpdateUnit(sortUnits[i].Id, sortUnits[i].Hp)
 							// TODO оповещалка об атаки в сокет
+							// TODO есть бага с отнимаение хп
 							UpdateUnit(sortUnits[i], activeUser)
 						}
 					}
@@ -100,7 +101,6 @@ func attack(sortUnits []objects.Unit, activeUser []*Clients) {
 }
 
 func UpdateUnit(unit objects.Unit, activeUser []*Clients)  {
-
 	for _, client := range activeUser {
 		if unit.NameUser == client.Login {
 			realUnit := client.Units[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
@@ -108,6 +108,7 @@ func UpdateUnit(unit objects.Unit, activeUser []*Clients)  {
 			unit.Watch = realUnit.Watch
 
 			client.Units[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)] = &unit
+
 			for _, units := range client.Units {
 				_, ok := units.WatchUnit[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
 				if ok { // TODO вынести одинаковые метод по поиску юнитов в отдельный метод, а то чето пиздец
@@ -118,16 +119,16 @@ func UpdateUnit(unit objects.Unit, activeUser []*Clients)  {
 			var unitsParametr = InitUnit{Event: "InitUnit", UserName: client.Login, TypeUnit: unit.NameType, UserOwned: unit.NameUser,
 				HP: unit.Hp, UnitAction: strconv.FormatBool(unit.Action), Target: "", X: unit.X, Y: unit.Y} // остылаем событие добавления юнита
 			initUnit <- unitsParametr
-
 		} else {
-
 			_, ok := client.HostileUnits[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
+
 			if ok {
 				client.HostileUnits[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)] = &unit
 				var unitsParametr = InitUnit{Event: "InitUnit", UserName: client.Login, TypeUnit: unit.NameType, UserOwned: unit.NameUser,
 					HP: unit.Hp, UnitAction: strconv.FormatBool(unit.Action), Target: "", X: unit.X, Y: unit.Y} // остылаем событие добавления юнита
 				initUnit <- unitsParametr
 			}
+
 			for _, units := range client.Units {
 				_, ok := units.WatchUnit[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
 				if ok {
@@ -139,52 +140,56 @@ func UpdateUnit(unit objects.Unit, activeUser []*Clients)  {
 }
 
 func DelUnit(unit objects.Unit, activeUser []*Clients) {
-
 	for _, client := range activeUser {
 		if unit.NameUser == client.Login {
+			_, ok := client.Units[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
 
-			realUnit := client.Units[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
-			unit.WatchUnit = realUnit.WatchUnit
-			unit.Watch = realUnit.Watch
-			delete(client.Units, strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y))
-			resp := Coordinate{Event: "OpenCoordinate", UserName: client.Login, X: unit.X, Y: unit.Y}
-			coordiante <- resp
+			if ok {
+				WatchUnit := client.Units[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)].WatchUnit
+				Watch := client.Units[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)].Watch
+				unit.WatchUnit = WatchUnit
+				unit.Watch = Watch
+				delete(client.Units, strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y))
 
-			for _, units := range client.Units {
-				_, ok := units.WatchUnit[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
-				if ok {
-					delete(units.WatchUnit, strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y))
-				}
-			}
+				resp := Coordinate{Event: "OpenCoordinate", UserName: client.Login, X: unit.X, Y: unit.Y}
+				coordiante <- resp
 
-			for _, coor := range realUnit.Watch {
-				del := true
 				for _, units := range client.Units {
-					_, ok := units.Watch[strconv.Itoa(coor.X)+":"+strconv.Itoa(coor.Y)]
+					_, ok := units.WatchUnit[strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y)]
 					if ok {
-						del = false
-						break
+						delete(units.WatchUnit, strconv.Itoa(unit.X)+":"+strconv.Itoa(unit.Y))
 					}
 				}
-				if del {
-					resp := Coordinate{Event: "DellCoordinate", UserName: client.Login, X: coor.X, Y: coor.Y} // то остылаем событие удаление юнита
-					coordiante <- resp
-				}
-			}
 
-			for _, hostile := range realUnit.WatchUnit { // TODO неуверен что тут это надо
-				del := true
-				for _, units := range client.Units {
-					_, ok := units.Watch[strconv.Itoa(hostile.X)+":"+strconv.Itoa(hostile.Y)]
-					if ok {
-						del = false
-						break
+				for _, coor := range unit.Watch {
+					del := true
+					for _, units := range client.Units {
+						_, ok := units.Watch[strconv.Itoa(coor.X)+":"+strconv.Itoa(coor.Y)]
+						if ok {
+							del = false
+							break
+						}
+					}
+					if del {
+						resp := Coordinate{Event: "DellCoordinate", UserName: client.Login, X: coor.X, Y: coor.Y}
+						coordiante <- resp
 					}
 				}
-				if del {
-					delete(client.HostileUnits, strconv.Itoa(hostile.X)+":"+strconv.Itoa(hostile.Y))
-					resp := Coordinate{Event: "DellCoordinate", UserName: client.Login, X: hostile.X, Y: hostile.Y} // то остылаем событие удаление юнита
-					coordiante <- resp
+
+				for _, hostile := range unit.WatchUnit {
+					del := true
+					for _, units := range client.Units {
+						_, ok := units.Watch[strconv.Itoa(hostile.X)+":"+strconv.Itoa(hostile.Y)]
+						if ok {
+							del = false
+							break
+						}
+					}
+					if del {
+						delete(client.HostileUnits, strconv.Itoa(hostile.X)+":"+strconv.Itoa(hostile.Y))
+						resp := Coordinate{Event: "DellCoordinate", UserName: client.Login, X: hostile.X, Y: hostile.Y}
+						coordiante <- resp
+					}
 				}
 			}
 		} else {
