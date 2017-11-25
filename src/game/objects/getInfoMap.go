5e76fcd6e90afd4604960667e8187881e64eea64
TrashPony <TrashPony13@gmail.com>
2017-11-25 21:28:54 +0300

    Перезалив проекта

---
+++ src/game/objects/getInfoMap.go
@@ -0,0 +1,33 @@
+package objects
+
+import (
+	"log"
+	"strconv"
+)
+
+func GetInfoMap(idMap int) Map {
+
+	rows, err := db.Query("Select * FROM map WHERE id =" + strconv.Itoa(idMap))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var mp Map
+	for rows.Next() {
+		err := rows.Scan(&mp.Id, &mp.Name, &mp.Xsize, &mp.Ysize, &mp.Type)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return mp
+}
+
+type Map struct {
+	Id    int
+	Name  string
+	Xsize int
+	Ysize int
+	Type  string
+}
