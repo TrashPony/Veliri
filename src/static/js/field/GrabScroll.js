84548e38a5792f27528cffd599d23406d08c74e0
rkhromov <trashpony13@gmail.com>
2017-12-26 17:17:08 +0300

    Добавил js код прокрутки страницы зажатием правой кнопкой мыши

---
+++ src/static/js/field/GrabScroll.js
@@ -0,0 +1,27 @@
+var dragHandler = {
+    lastClientX: 0,
+    start: function (e) {
+        if (e.button === 2) {
+            window.addEventListener('mousemove', dragHandler.drag);
+            dragHandler.lastClientX = e.clientX;
+            dragHandler.lastClientY = e.clientY;
+            e.preventDefault();
+        }
+    },
+    end: function (e) {
+        if (e.button === 2) {
+            window.removeEventListener('mousemove', dragHandler.drag);
+        }
+    },
+    drag: function (e) {
+        var deltaX = e.clientX - dragHandler.lastClientX;
+        var deltaY = e.clientY - dragHandler.lastClientY;
+        window.scrollTo(window.scrollX - deltaX, window.scrollY - deltaY);
+        dragHandler.lastClientX = e.clientX;
+        dragHandler.lastClientY = e.clientY;
+        e.preventDefault();
+    }
+};
+
+document.body.addEventListener('mousedown', dragHandler.start);
+document.body.addEventListener('mouseup', dragHandler.end);
