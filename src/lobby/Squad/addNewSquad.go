891e92fd15f34928e73c88050cc85a5b34f55c09
TrashPony <TrashPony13@gmail.com>
2018-04-27 20:22:24 +0300

    fix add new Squad

--- src/lobby/Squad/addNewSquad.go
+++ src/lobby/Squad/addNewSquad.go
@@ -4,18 +4,14 @@ import "log"
 
 func AddNewSquad(name string, userID int) (err error, squad *Squad) {
 	// TODO проверка на имя
-	res, err := db.Exec("INSERT INTO squads (name, id_user) VALUES ($1, $2)", name, userID)
+	id := 0
+	err = db.QueryRow("INSERT INTO squads (name, id_user) VALUES ($1, $2) RETURNING id", name, userID).Scan(&id)
+
 	if err != nil {
 		log.Fatal(err)
 		return err, nil
 	}
-
-	id, err := res.LastInsertId()
-	if err != nil {
-		log.Fatal(err)
-		return err, nil
-	}
-
+	
 	squad = &Squad{ID: int(id), Name:name}
 
 	return nil, squad
