9e8c1b670c24e8c8e1c0e17690e5adaea9da8786
TrashPony <TrashPony13@gmail.com>
2018-04-29 13:04:54 +0300

    пофиксил отображение окна сборки юнита

--- src/static/lobby/js/ConstructorsMenu/UnitConstructor.js
+++ src/static/lobby/js/ConstructorsMenu/UnitConstructor.js
@@ -11,13 +11,30 @@ function InitCreateUnit() {
         unitConstructor.id = "unitConstructor";
         lobbyMenu.appendChild(unitConstructor);
 
+        var constructorTable = document.createElement("table");
+        constructorTable.id = "constructorTable";
+        var constructorTr = document.createElement("tr");
+        var tdUnitParams = document.createElement("td");
+        tdUnitParams.className = "ConstructorTD";
+        var tdUnitMenu = document.createElement("td");
+        tdUnitMenu.className = "ConstructorTD";
+        var tdTabDetailMenu = document.createElement("td");
+        tdTabDetailMenu.className = "ConstructorTD";
+
+        constructorTable.appendChild(constructorTr);
+        constructorTr.appendChild(tdUnitParams);
+        constructorTr.appendChild(tdUnitMenu);
+        constructorTr.appendChild(tdTabDetailMenu);
+
         var unitMenu = CreateUnitMenu();
         var unitParams =  CreateUnitParams();
         var tabDetailMenu = CreateTabDetailMenu();
 
-        unitConstructor.appendChild(unitParams);
-        unitConstructor.appendChild(unitMenu);
-        unitConstructor.appendChild(tabDetailMenu);
+        tdUnitParams.appendChild(unitParams);
+        tdUnitMenu.appendChild(unitMenu);
+        tdTabDetailMenu.appendChild(tabDetailMenu);
+
+        unitConstructor.appendChild(constructorTable);
 
         lobby.send(JSON.stringify({
             event: "GetDetailOfUnits"
