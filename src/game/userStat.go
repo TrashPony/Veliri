a079346ef7da858ca634efb2b165264fa8cab260
Sam Smith <trashpony13@gmail.com>
2018-05-10 16:51:44 +0300

    get equip in userStat

--- src/game/userStat.go
+++ src/game/userStat.go
@@ -5,6 +5,7 @@ import (
 )
 
 type UserStat struct {
+	ID     int      `json:"id"`
 	IdGame int      `json:"id_game"`
 	Name   string   `json:"name"`
 	Ready  bool     `json:"ready"`
@@ -18,9 +19,11 @@ type Equip struct {
 
 func GetUserStat(idGame int) []*UserStat {
 
-	rows, err := db.Query("Select agu.id_game, users.name, agu.ready FROM action_game_user as agu, users WHERE agu.id_user=users.id AND agu.id_game=$1", idGame)
+	rows, err := db.Query("Select agu.id_game, users.name, agu.ready, users.id "+
+		"FROM action_game_user as agu, users "+
+		"WHERE agu.id_user=users.id AND agu.id_game=$1", idGame)
 	if err != nil {
-		println("gate game user stat")
+		println("get game user stat")
 		log.Fatal(err)
 	}
 	defer rows.Close()
@@ -28,16 +31,41 @@ func GetUserStat(idGame int) []*UserStat {
 	users := make([]*UserStat, 0)
 	for rows.Next() {
 		var user UserStat
-		err := rows.Scan(&user.IdGame, &user.Name, &user.Ready)
+		err := rows.Scan(&user.IdGame, &user.Name, &user.Ready, &user.ID)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		user.GetEquip()
 		users = append(users, &user)
 	}
 
 	return users
 }
 
-func (user *UserStat) GetEquip()  {
+func (user *UserStat) GetEquip() {
+	rows, err := db.Query("Select type.type, equip.used "+
+		"FROM action_game_equipping as equip, equipping_type as type, users "+
+		"WHERE type.id=equip.id_type AND equip.id_user=$1 AND equip.id_game=$2", user.ID, user.IdGame)
+	if err != nil {
+		println("get user equip stat")
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
+	equips := make([]*Equip, 0)
+	for rows.Next() {
+
+		var equip Equip
+
+		err := rows.Scan(&equip.Type, &equip.Used)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		user.GetEquip()
+		equips = append(equips, &equip)
+	}
+
+	user.Equip = equips
 }
