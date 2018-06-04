eb3f278435c65ce4be7033d2ba8845654fb11aba
TrashPony <TrashPony13@gmail.com>
2018-06-04 23:22:44 +0300

    анимация линий прицеливания

--- src/static/field/js/Map/SelectZone/AlphaSelect.js
+++ src/static/field/js/Map/SelectZone/AlphaSelect.js
@@ -1,4 +1,5 @@
 var alpha;
+var alphaTarget;
 
 function AlphaSelect() {
     if (alpha === undefined) {
@@ -16,6 +17,24 @@ function AlphaSelect() {
         game.SelectLineLayer.alpha = game.SelectLineLayer.alpha + 0.01;
         if (game.SelectLineLayer.alpha >= 0.8) {
             alpha = false
+        }
+    }
+
+    if (alphaTarget === undefined) {
+        alpha = false;
+    }
+
+    if (game.SelectTargetLineLayer.alpha >= 0.3 && !alpha) {
+        game.SelectTargetLineLayer.alpha = game.SelectTargetLineLayer.alpha - 0.005;
+        if (game.SelectTargetLineLayer.alpha < 0.3) {
+            alphaTarget = true;
+        }
+    }
+
+    if (alpha) {
+        game.SelectTargetLineLayer.alpha = game.SelectTargetLineLayer.alpha + 0.005;
+        if (game.SelectTargetLineLayer.alpha >= 0.8) {
+            alphaTarget = false
         }
     }
 }
