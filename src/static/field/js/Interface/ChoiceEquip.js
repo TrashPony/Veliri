43eb3aee59d584ed0d8342108416222692b0881c
TrashPony <TrashPony13@gmail.com>
2018-06-08 21:22:41 +0300

    менюшка инвентаря

--- src/static/field/js/Interface/ChoiceEquip.js
+++ src/static/field/js/Interface/ChoiceEquip.js
@@ -4,12 +4,60 @@ function ChoiceEquip() {
     if (!inventory) {
         inventory = document.createElement("div");
         inventory.id = "inventory";
+
+        var table = CreateTableInventory();
+        inventory.appendChild(table);
+        var cancelButton = CreateCancelButton();
+        inventory.appendChild(cancelButton);
+
     } else {
         inventory.style.display = "inline-block";
     }
-    //todo заголовок, кнопка отмены, заполнение
-    inventory.style.top = stylePositionParams.top - 87 + 'px';
+
+    inventory.style.top = stylePositionParams.top - 90 + 'px';
     inventory.style.left = 70 + stylePositionParams.left + 'px';
 
     document.body.appendChild(inventory);
+}
+
+function CreateTableInventory() {
+    var table = document.createElement("table");
+    table.id = "TableInventory";
+    var trHead = document.createElement("tr");
+    trHead.height = "10px";
+    var thHead = document.createElement("th");
+    thHead.colSpan = 4;
+    thHead.className = "h";
+    thHead.innerHTML = "Инвертарь";
+    thHead.appendChild(trHead);
+    table.appendChild(thHead);
+
+    for (var i = 0; i < 3; i++) {
+        var rowInventory = document.createElement("tr");
+        rowInventory.className = "rowInventory";
+        for (var j = 0; j < 4; j++) {
+            var cellInventory = document.createElement("td");
+            cellInventory.className = "cellInventory";
+            //todo заполнение итемами + onclick + mouseOver со вспылающей подсказкой
+            rowInventory.appendChild(cellInventory);
+        }
+        table.appendChild(rowInventory);
+    }
+
+    return table
+}
+
+function CreateCancelButton() {
+    var cancelButton = document.createElement("input");
+    cancelButton.type = "button";
+    cancelButton.value = "Отмена";
+    cancelButton.className = "button subMenu";
+
+    cancelButton.onclick = function () {
+        if (document.getElementById("inventory")) {
+            document.getElementById("inventory").remove()
+        }
+    };
+
+    return cancelButton;
 }
