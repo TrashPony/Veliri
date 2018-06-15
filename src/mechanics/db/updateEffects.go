be1c6c79157598550ccfb9075e2e3b4ad4a0a89c
TrashPony <TrashPony13@gmail.com>
2018-06-15 20:33:43 +0300

    метод добавления удаления и обновления эфектов в бд

--- src/mechanics/db/updateEffects.go
+++ src/mechanics/db/updateEffects.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"../unit"
+	"../coordinate"
 	"log"
 )
 
@@ -10,7 +11,7 @@ func UpdateUnitEffects(unit *unit.Unit) error {
 		if unitEffect.StepsTime == 0 {
 
 			_, err := db.Exec("DELETE FROM action_game_unit_effects WHERE id=$1", unitEffect.ID)
-			// todo
+
 			if err != nil {
 				println("Error delete unit effect")
 				log.Fatal(err)
@@ -40,6 +41,50 @@ func UpdateUnitEffects(unit *unit.Unit) error {
 				}
 
 				unitEffect.ID = id
+			}
+		}
+	}
+
+	return nil
+}
+
+func UpdateCoordinateEffects(mapCoordinate *coordinate.Coordinate) error {
+	for _, coordinateEffect := range mapCoordinate.Effects {
+		if coordinateEffect.StepsTime == 0 {
+
+			_, err := db.Exec("DELETE FROM action_game_zone_effects WHERE id=$1", coordinateEffect.ID)
+
+			if err != nil {
+				println("Error delete coordinate effect")
+				log.Fatal(err)
+				return err
+			}
+
+		} else {
+			if coordinateEffect.ID != 0 {
+
+				_, err := db.Exec("UPDATE action_game_zone_effects SET left_steps=$1, x=$2, y=$3 id_effect=$4 WHERE id=$5",
+					coordinateEffect.StepsTime, mapCoordinate.X, mapCoordinate.Y, coordinateEffect.TypeID, coordinateEffect.ID)
+
+				if err != nil {
+					println("Error update coordinate effect")
+					log.Fatal(err)
+					return err
+				}
+
+			} else {
+
+				id := 0
+				err := db.QueryRow("INSERT INTO action_game_zone_effects (id_game, id_effect, x, y, left_steps) "+
+					"VALUES ($1, $2, $3, $4, $5) RETURNING id", mapCoordinate.GameID, coordinateEffect.TypeID, mapCoordinate.X, mapCoordinate.Y, coordinateEffect.StepsTime).Scan(&id)
+
+				if err != nil {
+					println("Error add new coordinate effect")
+					log.Fatal(err)
+					return err
+				}
+
+				coordinateEffect.ID = id
 			}
 		}
 	}
