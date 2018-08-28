f4e45da676a88deb1f9d6c8de5b6452b3fb15441
trashpony <rfhnjirf1>
2018-08-28 12:29:02 +0300

    добавил немного информации о модулях в инвентаре при их выборе

--- src/static/assets/components/inventory/selected/SelectInventoryItem.js
+++ src/static/assets/components/inventory/selected/SelectInventoryItem.js
@@ -3,7 +3,7 @@ function SelectInventoryItem(e) {
     DestroyInventoryTip();
     DestroyInventoryClickEvent();
 
-    InventorySelectTip(JSON.parse(this.slotData).item, e.clientX, e.clientY);
+    InventorySelectTip(JSON.parse(this.slotData), e.clientX, e.clientY);
 
     if (JSON.parse(this.slotData).type === "body") {
         SetBody(JSON.parse(this.slotData).item, this.number);
--- src/static/assets/components/inventory/tip/SelectItem.js
+++ src/static/assets/components/inventory/tip/SelectItem.js
@@ -1,4 +1,4 @@
-function InventorySelectTip(item, x, y) {
+function InventorySelectTip(slot, x, y) {
     let tip = document.createElement("div");
     tip.style.top = y + "px";
     tip.style.left = x + "px";
@@ -6,8 +6,11 @@ function InventorySelectTip(item, x, y) {
 
     let name = document.createElement("span");
     name.className = "InventoryTipName";
-    name.innerHTML = item.name;
+    name.innerHTML = slot.item.name;
     tip.appendChild(name);
+
+    let paramsTable = CreateParamsTable(slot);
+    tip.appendChild(paramsTable);
 
     let cancelButton = document.createElement("input");
     cancelButton.type = "button";
@@ -23,4 +26,38 @@ function InventorySelectTip(item, x, y) {
     tip.appendChild(cancelButton);
 
     document.body.appendChild(tip);
+}
+
+function CreateParamsTable(slot) {
+    let table = document.createElement("table");
+    table.id = "paramsItem";
+
+    if (slot.type === "weapon" || slot.type === "equip") {
+        table.appendChild(createRow("Use power:", slot.item.power));
+        table.appendChild(createRow("HP:", slot.hp));
+    } else if (slot.type === "body") {
+        table.appendChild(createRow("Power:", slot.item.max_power));
+        table.appendChild(createRow("HP:", slot.hp));
+    } else if (slot.type === "ammo"){
+        table.appendChild(createRow("Type:", slot.item.type));
+        table.appendChild(createRow("Damage:", slot.item.damage));
+    }
+
+    function createRow(params, count) {
+        let tr = document.createElement("tr");
+
+        let td1 = document.createElement("td");
+        td1.innerHTML = "<span class='Value'>" + params + "</span>";
+        td1.style.border = "0px";
+        let td2 = document.createElement("td");
+        td2.innerHTML = "<span class='Success'>" + count + "</span>";
+        td2.style.border = "0px";
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+
+        return tr
+    }
+
+    return table;
 }
