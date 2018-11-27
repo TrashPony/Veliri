fcb4ec82c078bbe4ec8a27da60a4df3dcf090a9d
TrashPony <TrashPony13@gmail.com>
2018-11-27 10:09:34 +0300

    педелал смещение координат на фронтенде

--- src/static/game/CreateMap/Objects.js
+++ src/static/game/CreateMap/Objects.js
@@ -6,54 +6,58 @@ function CreateObjects(coordinate, x, y) {
     }
 
     if (coordinate.texture_object === "terrain_1") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "terrain_2") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "terrain_3") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "sand_stone_04") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "sand_stone_05") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "sand_stone_06") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "sand_stone_07") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "sand_stone_08") {
-        object = gameObjectCreate(x, y, coordinate.texture_object);
+        object = gameObjectCreate(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
     }
 
     if (coordinate.texture_object === "fallen_01") {
-        object = game.floorObjectLayer.create(x - 90, y + 10, coordinate.texture_object);
+        object = game.floorObjectLayer.create(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
+        object.anchor.setTo(0.5, 0.5);
         object.scale.set(0.4);
     }
 
     if (coordinate.texture_object === "fallen_02") {
-        object = game.floorObjectLayer.create(x - 95, y + 10, coordinate.texture_object);
+        object = game.floorObjectLayer.create(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
+        object.anchor.setTo(0.5, 0.5);
         object.scale.set(0.4);
     }
 
     if (coordinate.texture_object === "crater") {
-        object = game.floorObjectLayer.create(x - 90, y + 17, coordinate.texture_object);
+        object = game.floorObjectLayer.create(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
+        object.anchor.setTo(0.5, 0.5);
         object.scale.set(0.4);
     }
 
     if (coordinate.texture_object === "base") {
-        object = game.floorObjectLayer.create(x - 155, y - 50, coordinate.texture_object);
+        object = game.floorObjectLayer.create(coordinate.sprite.x, coordinate.sprite.y, coordinate.texture_object);
+        object.anchor.setTo(0.5, 0.5);
         object.scale.set(0.4);
     }
 
@@ -62,13 +66,13 @@ function CreateObjects(coordinate, x, y) {
 
 function gameObjectCreate(x, y, texture) {
 
-    let object = game.floorObjectLayer.create(x - 90, y + 10, texture);
+    let object = game.floorObjectLayer.create(x, y, texture);
+    object.anchor.setTo(0.5, 0.5);
     object.scale.set(0.4);
-    object.anchor.setTo(0, 0);
 
-    let shadow = game.floorObjectLayer.create(x - 90 + game.shadowXOffset, y - game.shadowYOffset + 25, texture);
+    let shadow = game.floorObjectLayer.create(x + game.shadowXOffset, y - game.shadowYOffset + 20, texture);
+    shadow.anchor.setTo(0.5, 0.5);
     shadow.scale.set(0.4);
-    shadow.anchor.setTo(0, 0);
     shadow.tint = 0x000000;
     shadow.alpha = 0.6;
 
--- src/static/game/CreateMap/Terrain.js
+++ src/static/game/CreateMap/Terrain.js
@@ -1,6 +1,7 @@
 function CreateTerrain(coordinate, x, y, q, r) {
 
     let floorSprite = game.floorLayer.create(x, y, coordinate.texture_flore);
+    floorSprite.anchor.setTo(0.5);
     floorSprite.scale.set(0.5);
 
     floorSprite.inputEnabled = true; // включаем ивенты на спрайт
@@ -13,6 +14,7 @@ function CreateTerrain(coordinate, x, y, q, r) {
 
     if (game && game.typeService === "battle") {
         let fogSprite = game.fogOfWar.create(x, y, 'FogOfWar');
+        fogSprite.anchor.setTo(0.5);
         fogSprite.scale.set(0.5);
         floorSprite.events.onInputDown.add(RemoveSelect);
         coordinate.fogSprite = fogSprite;
@@ -21,47 +23,53 @@ function CreateTerrain(coordinate, x, y, q, r) {
 
     if (game && game.typeService !== "battle") {
         let style = {font: "12px Arial", fill: "#606060", align: "center"};
-        game.add.text(x + 50, y + 20, q + "," + r, style, game.weaponEffectsLayer);
+        game.add.text(x-10, y - 40, q + "," + r, style, game.weaponEffectsLayer);
 
         let allow = {font: "12px Arial", fill: "#150bff", align: "center"};
         let noAllow = {font: "12px Arial", fill: "#ff2821", align: "center"};
 
         if (!(coordinate.move && coordinate.view && coordinate.attack)){
             if (coordinate.move) {
-                game.add.text(x + 25, y + 45, 'm', allow, game.weaponEffectsLayer);
+                game.add.text(floorSprite.x - 40, floorSprite.y - 25, 'm', allow, game.weaponEffectsLayer);
             } else {
-                game.add.text(x + 25, y + 45, 'm', noAllow, game.weaponEffectsLayer);
+                game.add.text(floorSprite.x - 40, floorSprite.y - 25, 'm', noAllow, game.weaponEffectsLayer);
             }
 
             if (coordinate.view) {
-                game.add.text(x + 40, y + 45, 'w', allow, game.weaponEffectsLayer);
+                game.add.text(floorSprite.x - 25, floorSprite.y - 25, 'w', allow, game.weaponEffectsLayer);
             } else {
-                game.add.text(x + 40, y + 45, 'w', noAllow, game.weaponEffectsLayer);
+                game.add.text(floorSprite.x - 25, floorSprite.y - 25, 'w', noAllow, game.weaponEffectsLayer);
             }
 
             if (coordinate.attack) {
-                game.add.text(x + 55, y + 45, 'a', allow, game.weaponEffectsLayer);
+                game.add.text(floorSprite.x- 10, floorSprite.y - 25, 'a', allow, game.weaponEffectsLayer);
             } else {
-                game.add.text(x + 55, y + 45, 'a', noAllow, game.weaponEffectsLayer);
+                game.add.text(floorSprite.x- 10, floorSprite.y - 25, 'a', noAllow, game.weaponEffectsLayer);
             }
         }
     }
 
+    if (coordinate.level === 1) {
+        let style = {font: "36px Arial", fill: "#35daff", align: "center"};
+        let label = game.add.text(-50, -25, coordinate.level, style);
+        floorSprite.addChild(label);
+    }
+
     if (coordinate.level === 3) {
         let style = {font: "36px Arial", fill: "#68ff59", align: "center"};
-        let label = game.add.text(50, 55, coordinate.level, style);
+        let label = game.add.text(-50, -25, coordinate.level, style);
         floorSprite.addChild(label);
     }
 
     if (coordinate.level === 4) {
         let style = {font: "36px Arial", fill: "#fff523", align: "center"};
-        let label = game.add.text(50, 55, coordinate.level, style);
+        let label = game.add.text(-50, -25, coordinate.level, style);
         floorSprite.addChild(label);
     }
 
     if (coordinate.level === 5) {
         let style = {font: "36px Arial", fill: "#ff2821", align: "center"};
-        let label = game.add.text(50, 55, coordinate.level, style);
+        let label = game.add.text(-50, -25, coordinate.level, style);
         floorSprite.addChild(label);
     }
 }
