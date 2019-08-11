d7fcbb45ff46c3e77b14c90ba9c6da0bdddebfeb
TrashPony <TrashPony13@gmail.com>
2019-08-11 12:54:55 +0300

    автопркрутка чата

--- static/assets/components/chat/Message.js
+++ static/assets/components/chat/Message.js
@@ -24,8 +24,13 @@ function chatMessage() {
 }
 
 function NewChatMessage(message, id) {
+
     if (id === currentChatID) {
+
         let chatBox = document.getElementById("chatBox");
+
+        // смотрим находится ли скрол в самом низу
+        let chatIsDown = chatBox.scrollTop === chatBox.scrollHeight - chatBox.clientHeight;
 
         if (!message.system) {
             let chatMessage = document.createElement("div");
@@ -45,44 +50,13 @@ function NewChatMessage(message, id) {
         } else {
             systemMessage(message.message)
         }
+        
+        // если скрол чата в самом низу то прокручивать его при новых сообщениях, если нет то без автопрокрутки
+        if (chatIsDown) chatBox.scrollTop = chatBox.scrollHeight - chatBox.clientHeight;
+
     } else {
         let chatTab = document.getElementById('chat' + id);
         if (chatTab) chatTab.className = 'alertChatTab';
-    }
-}
-
-let chatHide = false;
-
-function HideChat() {
-    let chat = document.getElementById("chat");
-    let chatBox = document.getElementById("chatBox");
-    let chatInput = document.getElementById("chatInput");
-    let usersBox = document.getElementById("usersBox");
-
-    function transform(el, height, opacity, sec) {
-        el.style.transition = sec + "s";
-        el.style.height = height + "px";
-        el.style.opacity = opacity;
-        setTimeout(function () {
-            el.style.transition = "0s";
-        }, 1000);
-    }
-
-    if (!chatHide) {
-        chat.style.bottom = "180px";
-        transform(chat, 20, 1, 1);
-        transform(chatBox, 0, 0, 0.5);
-        transform(chatInput, 0, 0, 0.5);
-        transform(usersBox, 0, 0, 0.5);
-
-        chatHide = true;
-    } else {
-        chat.style.bottom = "20px";
-        transform(chat, 200, 1, 1);
-        transform(chatBox, 125, 1, 1.5);
-        transform(chatInput, 30, 1, 1.5);
-        transform(usersBox, 125, 1, 1.5);
-        chatHide = false;
     }
 }
 
